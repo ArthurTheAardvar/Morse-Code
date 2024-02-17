8895d8e82049ee883843e31f399bf59a3cb5df39
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2024-02-17 11:28:29 -0700

    s

---
+++ Morse Code.py
@@ -0,0 +1,61 @@
+cases = int(input())
+
+for i in range(cases):
+    line = input()
+
+    if "A" in line:
+        print(". -")
+    if "B" in line:
+        print("- . . .")
+    if "C" in line:
+        print("- . - .")
+    if "D" in line:
+        print("- . .")
+    if "E" in line:
+        print(".")
+    if "F" in line:
+        print(". . - .")
+    if "G" in line:
+        print("- - .")
+    if "H" in line:
+        print(". . . .")
+    if "I" in line:
+        print(". .")
+    if "J" in line:
+        print(". - - -")
+    if "K" in line: 
+        print("- . -")
+    if "L" in line:
+        print(". - . .")
+    if "M" in line:
+        print("- -")
+    if "N" in line:
+        print("- .")
+    if "O" in line:
+        print("- - -")
+    if "P" in line:
+        print(". - - .")
+    if "Q" in line:
+        print("- - . -")
+    if "R" in line:
+        print(". - .")
+    if "S" in line:
+        print(". . .")
+    if "T" in line:
+        print("-")
+    if "U" in line:
+        print(". . -")
+    if "V" in line:
+        print(". . . -")
+    if "W" in line:
+        print(". - -")
+    if "X" in line:
+        print("- . . -")
+    if "Y" in line:
+        print("- . - -")
+    if "Z" in line:
+        print("- - . .")
+
+    
+
+    
